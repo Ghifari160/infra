5940b6dd4e84b39e28c56b901e819bdb760db53b
GHIFARI160 <ghifari@ghifari160.com>
2020-11-23 19:36:57 -0600

    Implement multitrack extraction to ffmpeg-render

--- renderer/scripts/ffmpeg-render.sh
+++ renderer/scripts/ffmpeg-render.sh
@@ -27,7 +27,47 @@ curl -d '{
     -H 'Content-Type: application/json' \
     $WEBHOOK_URL
 
+# Uncomment these lines to transcode the input with H265/HEVC in a MP4 container
+# ffmpeg -i ${1} \
+#     -movflags faststart \
+#     -c:v libx265 \
+#     -profile:v main \
+#     -preset faster \
+#     -bf 2 \
+#     -g 30 \
+#     -tag:v hvc1 \
+#     -x265-params open-gop=0 \
+#     -crf 18 \
+#     -pix_fmt yuv420p \
+#     -c:a libfdk_aac \
+#     -profile:a aac_low \
+#     -b:a 384k \
+#     ${2}
+
+# Comment these lines if you don't want the extract the 6 audio tracks (from OBS footage) before
+# transcoding the first audio track and the video track. The audio tracks are extracted as is, and
+# encoded into the MP4 container with AAC-LC. The video track is encoded with H265/HEVC
 ffmpeg -i ${1} \
+    -map 0:a:0 \
+    -c:a copy \
+    ${2}_track1.aac \
+    -map 0:a:1 \
+    -c:a copy \
+    ${2}_track2.aac \
+    -map 0:a:2 \
+    -c:a copy \
+    ${2}_track3.aac \
+    -map 0:a:3 \
+    -c:a copy \
+    ${2}_track4.aac \
+    -map 0:a:4 \
+    -c:a copy \
+    ${2}_track5.aac \
+    -map 0:a:5 \
+    -c:a copy \
+    ${2}_track6.aac \
+    -map 0:v \
+    -map 0:a:0 \
     -movflags faststart \
     -c:v libx265 \
     -profile:v main \
